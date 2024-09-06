09e4fa17345411153b27e9ca3caf59a07a889539
Vitor <vitorgabrielb47@gmail.com>
2024-09-05 21:33:26 -0300

    Adicionado classe de Tratamento #3

--- aulas/models.py
+++ aulas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Tratamento(models.Model):
+    tratamento_recomendado = models.CharField(max_length=80)
+    medicacao =  models.CharField(max_length=80)
+    fisioterapia = models.CharField(max_length=3)
+    castracao = models.CharField(max_length=3)
