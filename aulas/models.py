8d112ff19c734be5dbb485d37c09cb93baf02c31
Thauã Kaike <tsouzamilan@gmail.com>
2024-09-05 21:40:38 -0300

    Criado classe de Diagnóstico

--- aulas/models.py
+++ aulas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Diagnostico(model.Model):
+    tratamento_recomendado = models.CharField(max_length=80)
+    observacao = models.CharField(max_length=999)
+    medicacao = models.CharField(max_length=80)
+    id_animal = models.IntegerField
